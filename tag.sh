126cbac1d60ce3b0998a59d5b07f86c7b8e972db
Ali-Akber Saifee <ali@indydevs.org>
2013-11-30 13:10:14 +0800

    tagging script

---
+++ tag.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+echo current version:$(python -c "import hiro.version;print hiro.version.__version__")
+read -p "new version:" new_version
+sed -i -e "s/__version__.*/__version__=\"${new_version}\" # pragma: no cover/g" hiro/version.py 
+echo "tagging $new_version"
+git add hiro/version.py
+git commit -m "updating version to ${new_version}"
+git tag -s $(python setup.py --version) -m "tagging version ${new_version}"
+python setup.py build sdist bdist_egg upload
+
+
