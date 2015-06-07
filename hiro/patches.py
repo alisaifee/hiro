1502474df423d6af4ec422f0d58db2d5e4c34a3d
Ali-Akber Saifee <ali@indydevs.org>
2015-06-08 07:17:29 +0800

    Use abcmeta & six.add_metaclass for patches

--- hiro/patches.py
+++ hiro/patches.py
@@ -1,11 +1,14 @@
 """
 patched builtin time classes for use by :class:`hiro.Timeline`
 """
+import abc
 from datetime import date as realdate
 from datetime import datetime as realdatetime
 import time
+import six
 
-class DatetimeMeta(type):
+
+class DatetimeMeta(abc.ABCMeta):
     """
     meta class to allow interaction between :class:`datetime.datetime`
     objects create inside the :class:`hiro.Timeline` with those created
@@ -23,12 +26,13 @@ class DateMeta(type):
     def __instancecheck__(cls, instance):
         return isinstance(instance, realdate)
 
+@six.add_metaclass(DatetimeMeta)
 class Datetime(realdatetime):
     """
     used to patch :class:`datetime.datetime` to follow the rules of the
     parent :class:`hiro.Timeline`
     """
-    __metaclass__ = DatetimeMeta
+
     @classmethod
     def now(cls, tz=None):
         return cls.fromtimestamp(time.time(), tz)
@@ -37,12 +41,14 @@ class Datetime(realdatetime):
     def utcnow(cls):
         return cls.fromtimestamp(time.mktime(time.gmtime()))
 
+@six.add_metaclass(DateMeta)
 class Date(realdate):
     """
     used to patch :class:`datetime.date` to follow the rules of the
     parent :class:`hiro.Timeline`
     """
     __metaclass__ = DateMeta
+
     @classmethod
     def today(cls):
         return cls.fromtimestamp(time.time())
