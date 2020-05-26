8f72f4d7c95b8e209156b04cfe730043ed765909
Ali-Akber Saifee <ali@indydevs.org>
2020-05-25 17:40:28 -0700

    Convert tests to use pytest

--- tests/test_runners.py
+++ tests/test_runners.py
@@ -1,57 +1,63 @@
 """
 
 """
-import unittest
 import time
 import hiro
 from hiro.errors import SegmentNotComplete
+import pytest
 
 
-class TestSyncRunner(unittest.TestCase):
-    def test_scale_up_runner(self):
-        def _slow_func():
-            time.sleep(1)
-            return 1
-        f = hiro.run_sync(4, _slow_func)
-        self.assertTrue(f.get_execution_time() < 1)
-        self.assertEquals(f.get_response(), 1)
-
-    def test_scale_up_runner_fail(self):
-        def _slow_func():
-            time.sleep(1)
-            raise Exception("foo")
-        f = hiro.run_sync(4, _slow_func)
-        self.assertRaises(Exception, f.get_response)
-        self.assertTrue(f.get_execution_time() < 1)
+def test_scale_up_sync_runner():
+    def _slow_func():
+        time.sleep(1)
+        return 1
+    f = hiro.run_sync(4, _slow_func)
+    assert f.get_execution_time() < 1
+    assert f.get_response() == 1
 
 
-class TestASyncRunner(unittest.TestCase):
-    def test_scale_up_runner(self):
-        def _slow_func():
-            time.sleep(1)
-        f = hiro.run_async(4, _slow_func)
-        self.assertTrue(f.is_running())
-        f.join()
-        self.assertTrue(f.get_execution_time() < 1)
+def test_scale_up_sync_runner_fail():
+    def _slow_func():
+        time.sleep(1)
+        raise Exception("foo")
+    f = hiro.run_sync(4, _slow_func)
+    with pytest.raises(Exception):
+        f.get_response()
+    assert f.get_execution_time() < 1
 
-    def test_scale_up_runner_fail(self):
-        def _slow_func():
-            time.sleep(1)
-            raise Exception("foo")
-        f = hiro.run_async(4, _slow_func)
-        self.assertTrue(f.is_running())
-        f.join()
-        self.assertRaises(Exception, f.get_response)
-        self.assertTrue(f.get_execution_time() < 1)
 
-    def test_segment_not_complete_error(self):
-        def _slow_func():
-            time.sleep(1)
-            raise Exception("foo")
-        f = hiro.run_async(4, _slow_func)
-        self.assertRaises(SegmentNotComplete, f.get_execution_time)
-        self.assertRaises(SegmentNotComplete, f.get_response)
-        self.assertTrue(f.is_running())
-        f.join()
-        self.assertRaises(Exception, f.get_response)
-        self.assertTrue(f.get_execution_time() < 1)
+def test_scale_up_async_runner():
+    def _slow_func():
+        time.sleep(1)
+    f = hiro.run_async(4, _slow_func)
+    assert f.is_running()
+    f.join()
+    assert f.get_execution_time() < 1
+
+
+def test_scale_up_async_runner_fail():
+    def _slow_func():
+        time.sleep(1)
+        raise Exception("foo")
+    f = hiro.run_async(4, _slow_func)
+    assert f.is_running()
+    f.join()
+    with pytest.raises(Exception):
+        f.get_response()
+    assert f.get_execution_time() < 1
+
+
+def test_segment_not_complete_error():
+    def _slow_func():
+        time.sleep(1)
+        raise Exception("foo")
+    f = hiro.run_async(4, _slow_func)
+    with pytest.raises(SegmentNotComplete):
+        f.get_execution_time()
+    with pytest.raises(SegmentNotComplete):
+        f.get_response()
+    assert f.is_running()
+    f.join()
+    with pytest.raises(Exception):
+        f.get_response()
+    assert f.get_execution_time() < 1
